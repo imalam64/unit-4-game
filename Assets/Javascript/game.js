85f38074e559304ec2603b0e1cdfc516d3989ff9
imalam64 <imalam64@gmail.com>
2018-06-17 15:43:50 -0700

    created html layout, found images, started JS

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,31 @@
+//setting initial variables to track at start of the game
+var wins =0;
+var losses = 0;
+var userScore = 0;
+
+//this is for the first part of the code that will 
+//generate a random number for the user to add up to
+var compNumber = Math.floor(Math.random() * 120) + 19;
+
+//this will display that number to the user
+
+//this will generate a random number for the crystals
+var cryst1 = Math.floor(Math.random() * 12) + 1;
+var cryst2 = Math.floor(Math.random() * 12) + 1;
+var cryst3 = Math.floor(Math.random() * 12) + 1;
+var cryst4 = Math.floor(Math.random() * 12) + 1;
+
+//assigning variable values to the images
+
+//waits for event click from user
+
+//adds the clicks to the userScore variable
+
+//conditional statement to check whether score is equal to or over 
+//the set compNumber
+
+    //add point to Wins
+
+    //add point to Losses
+
+    //resets the game for new round with either conditional
